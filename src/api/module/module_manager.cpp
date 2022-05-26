d74a86b60e92307ef366a7c2b5c841cb27d1eed7
Rina <37088203+SirRina@users.noreply.github.com>
2022-05-26 18:57:59 -0300

    Minimal update.

---
+++ src/api/module/module_manager.cpp
@@ -0,0 +1,25 @@
+#include "module_manager.h"
+
+void module_manager::on_start() {
+
+}
+
+void module_manager::on_end() {
+
+}
+
+void module_manager::on_event(SDL_Event sdl_event) {
+
+}
+
+void module_manager::on_locked_update(uint64_t delta) {
+
+}
+
+void module_manager::on_update(uint64_t delta) {
+
+}
+
+void module_manager::on_render(float render_time) {
+    
+}
