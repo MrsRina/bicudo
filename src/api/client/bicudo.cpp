79f7c896d5cf9a70054a738db13390a368fe59aa
Rina <37088203+SirRina@users.noreply.github.com>
2022-08-01 19:04:18 -0300

    No, I do not care

--- src/api/client/bicudo.cpp
+++ src/api/client/bicudo.cpp
@@ -91,6 +91,7 @@ void game_core::init_window() {
     SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
     SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 3);
     SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 4);
+    SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
     SDL_GL_SetSwapInterval(1);
 }
 
